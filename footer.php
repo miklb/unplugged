d780ecc106c50a95ad223dfbf85a7dda50e37b71
Michael Bishop <miklb@miklb.com>
2018-03-30 00:31:41 -0400

    add footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * The header for our theme.
+	 *
+	 * This is the template that displays all of the <head> section as well as header & nav.
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+	 *
+	 * @package unplugged
+	 */
+
+?>
+		</div><!-- end wrapper -->
+		<footer class="footer">
+			<p>
+			Built on <a href="https://indieweb.org">IndieWeb</a> principles • The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+			</p>
+		<?php wp_footer(); ?>
+		</footer>
+	</body>
+</html>
