d684b4826503b69ce433aa3428a9e1d9ef7b34d1
Michael Bishop <miklb@miklb.com>
2018-06-12 22:49:28 -0400

    chore: formatting; empty stylesheet

--- header.php
+++ header.php
@@ -9,26 +9,28 @@
 	 * @package unplugged
 	 */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-	<div class="wrapper">
-		<header class="banner">
-			<nav class="navbar" role="navigation" aria-label="main navigation">
-				<div class="navbar-brand">
-					<a class="navbar-item" href="<?php bloginfo( 'url' ); ?>">
-						<?php bloginfo( 'title' ); ?>
-					</a>
-				</div>
-				<?php
+?>
+	<!DOCTYPE html>
+	<html <?php language_attributes(); ?>>
+
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div class="wrapper">
+			<header class="banner">
+				<nav class="navbar" role="navigation" aria-label="main navigation">
+					<div class="navbar-brand">
+						<a class="navbar-item" href="<?php bloginfo( 'url' ); ?>">
+							<?php bloginfo( 'title' ); ?>
+						</a>
+					</div>
+					<?php
 					//TODO: Feature: Add dynamic nav menu.
 				?>
-			</nav>
-		</header>
-	
+				</nav>
+			</header>
